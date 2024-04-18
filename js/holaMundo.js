1898f4b587120a9cafbe9844b9d8c062307c024b
Cecilia <cefernandez92@gmail.com>
2024-04-17 22:34:55 -0300

    uñtimos cambios

---
+++ js/holaMundo.js
@@ -0,0 +1,6 @@
+alert("Estas buscando...")
+const botonEnHtml= document.getElementById("btnBuscar")
+botonEnHtml.addEventListener('Presione el boton')
+function buscar(){
+    alert('Presiono el boton buscar...')
+}
